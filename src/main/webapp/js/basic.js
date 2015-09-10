3af34b213c47cd77d4b57f705da98c9f72f62717
lvwang <lvwangbeta@gmail.com>
2015-09-10 20:53:42 +0800

    输入域js安全控制

--- src/main/webapp/js/basic.js
+++ src/main/webapp/js/basic.js
@@ -3,17 +3,8 @@ img_base_url= 'http://7xkkim.com1.z0.glb.clouddn.com/';
 post_cover_thumbnail='?imageView2/2/w/500';
 album_thumbnail='?imageView2/1/w/200/h/200';
 
-		var current_version = 'develop';
-var osf = {
-		develop:{
-			basePath:'/com.lvwang.osf',
-			post_cover_style:null,
-			album_thumbnail:null
-		},
-		release:{
-			basePath:'',
-			post_cover_style:'@!postcover',
-			album_thumbnail:'@!albumthumbnail'
-		}
-		
+function escape(v) {
+    var  entry = { "'": "&apos;", '"': '&quot;', '<': '&lt;', '>': '&gt;' };
+    v = v.replace(/(['")-><&\\\/\.])/g, function ($0) { return entry[$0] || $0; });
+    return v;
 }
--- src/main/webapp/js/spost.js
+++ src/main/webapp/js/spost.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('#spost_send').live('click',function() {
-		var content = $('#spost_content').val();
+		var content = escape($('#spost_content').val());
 
 		$.ajax({
 			url: basePath + '/spost/create',
@@ -19,7 +19,7 @@ $(document).ready(function(){
 				$(short_post).find('img:first').attr('src', img_base_url + data.avatar);
 				$(short_post).find('.summary > a').attr('href', basePath+'/user/'+data.spost.post_author);
 				$(short_post).find('.summary > a').text(data.author_name);
-				$(short_post).find('.extra').text(data.spost.post_content);
+				$(short_post).find('.extra').html(data.spost.post_content);
 				$(short_post).find('.like > i').attr('object_id', data.spost.id);
 
 				$(short_post).css('display', 'block');
